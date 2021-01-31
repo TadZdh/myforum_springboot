1695eb86dbe5fe9df9ce76fcac481c3b6cac9056
TadZdh <1263705530@qq.com>
2021-01-31 20:34:10 +0800

    commit

--- src/main/java/com/example/myforum_springboot/controller/EchartsController.java
+++ src/main/java/com/example/myforum_springboot/controller/EchartsController.java
@@ -61,17 +61,4 @@ public class EchartsController {
         list.add(new Echarts("正常用户", normalUserCount));
         return list;
     }
-
-    @RequestMapping("/abcdef")
-    @ResponseBody
-    public void abcdef() {
-        System.out.println("11111111111111111");
-        System.out.println("22222222222222222");
-        System.out.println("33333333333333333");
-        System.out.println("44444444444444444");
-        System.out.println("33333333333333333");
-        System.out.println("44444444444444444");
-        System.out.println("33333333333333333");
-        System.out.println("44444444444444444");
-    }
 }
