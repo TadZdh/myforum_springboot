7115eec26e30446834ac38aa78403fd241f9ab99
TadZdh <1263705530@qq.com>
2021-01-31 20:33:10 +0800

    commit

--- src/main/java/com/example/myforum_springboot/controller/EchartsController.java
+++ src/main/java/com/example/myforum_springboot/controller/EchartsController.java
@@ -65,15 +65,6 @@ public class EchartsController {
     @RequestMapping("/abcdef")
     @ResponseBody
     public void abcdef() {
-        System.out.println("11111111111111111");
-        System.out.println("22222222222222222");
-        System.out.println("33333333333333333");
-        System.out.println("44444444444444444");
-        System.out.println("33333333333333333");
-        System.out.println("44444444444444444");
-        System.out.println("33333333333333333");
-        System.out.println("44444444444444444");
-
-        System.out.println("*****************修复shop的bug");
+        System.out.println("*****************修复banner的bug");
     }
 }
