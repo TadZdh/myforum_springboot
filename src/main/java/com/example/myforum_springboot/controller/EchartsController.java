572c0a089fdc35ddc0ed093a4b98ea1ea23799c2
TadZdh <1263705530@qq.com>
2021-01-31 20:13:19 +0800

    commit

--- src/main/java/com/example/myforum_springboot/controller/EchartsController.java
+++ src/main/java/com/example/myforum_springboot/controller/EchartsController.java
@@ -62,9 +62,12 @@ public class EchartsController {
         return list;
     }
 
-    @RequestMapping("/abc")
+    @RequestMapping("/abcdef")
     @ResponseBody
-    public void abc() {
+    public void abcdef() {
         System.out.println("11111111111111111");
+        System.out.println("22222222222222222");
+        System.out.println("33333333333333333");
+        System.out.println("44444444444444444");
     }
 }
